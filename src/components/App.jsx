aae0abf5ea689b50fe14e4e63de968b3a58d8822
Luis Zubia <soyzubia@gmail.com>
2024-03-13 05:29:50 -0700

    Make the App show the Login/Signup links, and respond to routes login/signup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Links from './Links';
 import Login from './Login';
+import Signup from './Signup';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route index element={<Login />} />
+      <Route index element={<Links />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
     </Routes>
   </BrowserRouter>
 );
