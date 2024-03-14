2147d9565ce830288e36d2c296f998aece8cc805
Luis Zubia <soyzubia@gmail.com>
2024-03-14 02:22:27 -0700

    Create Login form component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,38 @@
+import { useState } from 'react';
+
 const Login = () => {
-  const title = 'Login';
+  const [data, setData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
 
   return (
-    <h2>{title}</h2>
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <div className="field row wrap">
+        <label className="lbl full" htmlFor="email">Email:</label>
+        <input className="grow" type="text" name="email" autoComplete autoFocus value={data.email} onChange={handleChange}/>
+      </div>
+
+      <div className="field row wrap">
+        <label className="lbl full" htmlFor="password">Password:</label>
+        <input className="grow" type="password" name="password" value={data.password} onChange={handleChange}/>
+      </div>
+
+      <button className="btn btn--primary" type="submit">Login</button>
+    </form>
   );
 }
 
